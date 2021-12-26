ad46037721b5d4b120e4d2f54b593a67d55e473c
elif rumeysa aydin <elifrumeysaaydin@gmail.com>
2021-12-26 06:54:44 +0300

    musteri

--- Proje/Data/ApplicationDbContext.cs
+++ Proje/Data/ApplicationDbContext.cs
@@ -7,20 +7,21 @@ using System.Text;
 
 namespace Proje.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<Musteri>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
 
-    
-        public DbSet<Kategori> Kategori { get; set; }
         public DbSet<Yemek> Yemek { get; set; }
-        public DbSet<YemekMalzeme> MalzemeYemek { get; set; }
+
+        public DbSet<Kategori> Kategori { get; set; }
 
         public DbSet<Malzeme> Malzeme { get; set; }
 
+        public DbSet<YemekMalzeme> YemekMalzeme { get; set; }
+
+
     }
 }
-
